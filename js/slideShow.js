2b4848dee6a1308f42596c22b3e4da49dd6c42de
Sourav Bagchi <bagchi.srv@gmail.com>
2021-06-25 03:27:35 +0530

    Updated Slideshow container
    -set styles for slideshow container
    - minor styling changes

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,15 +1,7 @@
 let slideNumber = 1;
 
-const nextSlide = x => {
-    renderSlide(slideNumber += x);
-}
-
-const thisSlide = x => {
-    renderSlide(x);
-}
 const renderSlide = x => {
     let slideList = document.getElementsByClassName('slides');
-    let dotList = document.getElementsByClassName('circle');
     if (x > slideList.length) slideNumber = 1;
     if (x < 1) slideNumber = slideList.length;
     
@@ -17,12 +9,29 @@ const renderSlide = x => {
         slideList[i].style.display = 'none';
     }
     
-    for (let j = 0; j < slideList.length; j++) {
-        dotList[j].className = dotList[j].className.replace(' active', '');
-    }
     slideList[slideNumber - 1].style.display = 'block';
-    dotList[slideNumber - 1].className += ' active';
+    
+    
 }
 
+const thisSlide = x => {
+    renderSlide(x);
+}
 
+const nextSlide = x => {
+    renderSlide(slideNumber += x);
+}
+
+const autoSlideShow = (x) => {
+    let i = 0;
+    while(i < x) {
+        for(let j = 1; j < 4; j++) {
+            setTimeout(renderSlide(j), 2000);
+        }
+        i++;
+    }    
+}
 renderSlide(slideNumber);
+autoSlideShow(100);
+
+
